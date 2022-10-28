326f7ffc9a85cbb6048e577123ed5ffaaf23d62b
MOONcitizenX <mooncitizenx@gmail.com>
2022-10-29 00:02:52 +0300

    feat: finish 01 read file

---
+++ 01-read-file/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const stream = new fs.ReadStream(path.resolve(__dirname, 'text.txt'), {
+	encoding: 'utf-8'
+});
+
+stream.on('readable', () => {
+	const data = stream.read();
+	if (data !== null) console.log(data);
+});
+
+stream.on('error', (err) => {
+	if (err.code === 'ENOENT') {
+		console.log('File not found');
+	} else {
+		console.log(err);
+	}
+});
