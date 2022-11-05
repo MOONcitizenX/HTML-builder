038c6aa1741791de15268965107c258fbb527ced
MOONcitizenX <mooncitizenx@gmail.com>
2022-11-05 14:03:17 +0300

    fix: fix 04-copy-directory to use fs.promises and remove existing folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
-const copyDir = () => {
-	const distPath = path.resolve(__dirname, 'files-copy');
-	const origPath = path.resolve(__dirname, 'files');
-	fs.mkdir(distPath, { recursive: true }, (err) => {
-		if (err) throw err;
-	});
-	fs.readdir(origPath, { withFileTypes: true }, (err, files) => {
-		if (err) throw err;
-		for (let file of files) {
-			if (file.isFile()) {
-				fs.copyFile(
-					path.join(origPath, file.name),
-					path.join(distPath, file.name),
-					(err) => {
-						if (err) throw err;
-					}
-				);
-			}
+const distPath = path.resolve(__dirname, 'files-copy');
+const origPath = path.resolve(__dirname, 'files');
+
+const copyDir = async (src, dist) => {
+	await fs.promises.rm(dist, { recursive: true, force: true });
+
+	const files = await fs.promises.readdir(src, { withFileTypes: true });
+	await fs.promises.mkdir(dist, { recursive: true });
+	for (let file of files) {
+		const srcPath = path.join(src, file.name);
+		const distPath = path.join(dist, file.name);
+		if (file.isDirectory()) {
+			await copyDir(srcPath, distPath);
+		} else {
+			await fs.promises.copyFile(srcPath, distPath);
 		}
-	});
+	}
 };
 
-copyDir();
+copyDir(origPath, distPath);
