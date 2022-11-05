737a683b3f4e1543925fa2ebec613ea0bbb49ef1
MOONcitizenX <mooncitizenx@gmail.com>
2022-11-05 12:53:43 +0300

    fix: fix 03-files-in-folder file name to work correctly with multidot file names

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,24 +2,20 @@ const { readdir } = require('fs');
 const fs = require('fs');
 const path = require('path');
 
-readdir(
-	path.resolve(__dirname, 'secret-folder'),
-	{ withFileTypes: true },
-	(err, files) => {
-		if (err) throw err;
-		const direntArr = files.filter((file) => file.isFile());
-		direntArr.forEach((file) => {
-			const path2file = path.join(__dirname, 'secret-folder', file.name);
-			fs.stat(path2file, (err, stats) => {
-				if (err) throw err;
-				console.log(
-					// path filename
+const src = path.resolve(__dirname, 'secret-folder');
 
-					`${file.name.split('.').slice(0, -1).join('')} - ${path
-						.extname(path2file)
-						.slice(1)} - ${(stats.size / 1000).toFixed(2)} kB`
-				);
-			});
+readdir(src, { withFileTypes: true }, (err, files) => {
+	if (err) throw err;
+	const direntArr = files.filter((file) => file.isFile());
+	direntArr.forEach((file) => {
+		const path2file = path.join(src, file.name);
+		fs.stat(path2file, (err, stats) => {
+			if (err) throw err;
+			console.log(
+				`${path.basename(path2file, path.extname(path2file))} - ${path
+					.extname(path2file)
+					.slice(1)} - ${stats.size} bytes`
+			);
 		});
-	}
-);
+	});
+});
