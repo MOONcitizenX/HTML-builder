05769e9fa0cb1a28c5fda245751a2f1520e7c873
MOONcitizenX <mooncitizenx@gmail.com>
2022-11-05 13:19:04 +0300

    fix: fix 02-write-file to handle big text chunks

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,30 +3,19 @@ const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
 const path = require('path');
 
+const distPath = path.resolve(__dirname, 'text.txt');
+
 const rl = readline.createInterface({ input, output });
-
-// fs.writeFile(path.resolve(__dirname, 'text.txt'), ``, (err) => {
-// 	if (err) console.log(err);
-// });
-
-// writeableStream
+const fileStream = fs.createWriteStream(distPath);
 
 rl.question('Enter your text: \n', (answer) => {
-	fs.appendFile(path.resolve(__dirname, 'text.txt'), answer + '\n', (err) => {
-		if (err) console.log(err);
-	});
+	fileStream.write(`${answer}\n`);
 	rl.on('line', (line) => {
 		if (line === 'exit') {
 			console.log('\nThank you! Goodbye!');
 			process.exit();
 		} else {
-			fs.appendFile(
-				path.resolve(__dirname, 'text.txt'),
-				line + '\n',
-				(err) => {
-					if (err) console.log(err);
-				}
-			);
+			fileStream.write(`${line}\n`);
 		}
 	});
 });
